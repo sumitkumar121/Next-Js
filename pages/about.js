31afd02c05fce01f1b18592c31088cd829f32b5d
fw-htp <htp@freshworks.io>
2018-04-10 12:45:39 +0530

    first commit

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import Layout from './components/MyLayout.js'
+export default () => (
+  <div>
+   <Layout>
+    <p>This is the about page</p>
+	 </Layout>
+  </div>
+)
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import Layout from './components/MyLayout.js'
+
+
+const Index = () => (
+  <div>
+   
+   <Layout>
+       <p>Hello Next.js</p>
+    </Layout>
+  </div>
+)
+
+export default Index
